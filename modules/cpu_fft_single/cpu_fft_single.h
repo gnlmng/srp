79c6a6eb96398354f16d50f13dc9be817574b897
gnlmng <102802930+gnlmng@users.noreply.github.com>
2022-04-01 13:27:04 +0200

    Create cpu_fft_single.h

---
+++ modules/cpu_fft_single/cpu_fft_single.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "../../util/timer.h"
+
+#include <vector>
+#include <complex>
+
+namespace cpu_fft_single {
+	
+	timestamp run(std::vector <std::complex <double>>& input);
+	
+} /// namespace cpu_fft_single
